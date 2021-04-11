e1cf43b14d71a7a8c66ae071546096d7d6f31d31
dhaama <hengmei8374@126.com>
2021-04-11 11:37:06 +0800

    Android & Windows Local Done :rocket:

--- index.android.js
+++ index.android.js
@@ -5,27 +5,67 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, ScrollView, ListView } from 'react-native';
 
 export default class appReact extends Component {
+
+  constructor(props) {
+    super(props);
+  
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+
+    this.state = {
+      data: ds.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+
+    this.getMovieData();
+  }
+
+  getMovieData(){
+    fetch('https://facebook.github.io/react-native/movies.json')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            const data = responseJson.movies;
+            console.log(data)
+            this.setState({
+              data: this.state.data.cloneWithRows(data)
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+  }
+
+
+
   render() {
+
+    const { data } = this.state
+    console.log('data', data)
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={[styles.first, styles.common]}>
+          <Text >
+            First
+          </Text>
+        </View>
+        <View style={[styles.second, styles.common]}>
+          <ListView 
+            dataSource={this.state.data}
+            renderRow={(item) => <Text>{item.title}+++{item.releaseYear}</Text>}
+          />
+        </View>
+          <View style={[styles.third, styles.common]}>
+          <Text>
+            Third{'\n'}
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </View>
       </View>
     );
   }
@@ -34,12 +74,26 @@ export default class appReact extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //flexDirection:'row',
+  },
+  first:{
+    flex:1,
+    backgroundColor: '#ccc',
+  },
+  second:{
+    flex:8,
+    backgroundColor: '#FFF',
+  },
+  third:{
+    flex:1,
+    backgroundColor: '#ccc',
+  },
+  common:{
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
   },
